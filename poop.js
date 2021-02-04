9b1ee343d43ee62f1b6761fb976a94d2a3e8efbc
toswm128 <chominsu10@gmail.com>
2021-02-05 00:46:46 +0900

    대충완성

--- poop.js
+++ poop.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const score = document.querySelector("h1");
+const body = document.querySelector("body");
 
 const img = new Image();
 img.src = "/혁준홍.png"
@@ -16,12 +18,26 @@ let y= maxH-183;
 let poopPoint;
 let i=0;
 
+let point = -1;
+
+function touchRead(){
+    if(poopPoint>x&&poopPoint<x+75){
+        if(i>y){
+            console.log("게임오버");
+            const over = document.createElement("h1");
+            body.appendChild(over);
+            over.innerText = `게임 오버 당신의 점수는 ${point}입니다.`
+        }
+    }
+}
+
 function poop(){
     i=0;
     poopPoint = Math.floor(Math.random()*maxW);
     console.log(poopPoint);
     ctx.strokeStyle="red"
-
+    point++;
+    score.innerText = point;
 }
 
 function poopDraw(){
@@ -33,7 +49,7 @@ function poopDraw(){
 function mouse(e){
     const mouseX = e.offsetX; 
     const mouseY = e.offsetY; 
-    x = mouseX-25;
+    x = mouseX-37.5;
     
 }
 
@@ -44,7 +60,7 @@ function draw(){
     ctx.stroke();
     poopDraw();
     if(i<maxH+125){
-        i=i+5;
+        i=i+3;
     }else{
         poop();
     }
@@ -53,6 +69,7 @@ function draw(){
 function init(){
     canvas.addEventListener("mousemove",mouse)
     setInterval(draw,1);
+    setInterval(touchRead,1);
     poop();
 }
 
