0e4bb0474b732b57c003296366cbb671c258c479
toswm128 <chominsu10@gmail.com>
2021-02-05 00:02:40 +0900

    똥피하기 쉬움

--- poop.js
+++ poop.js
@@ -10,11 +10,28 @@ const maxH = canvas.height;
 let x=0;
 let y= maxH-101;
 
+let poopPoint;
+let i=0;
+
+function poop(){
+    i=0;
+    poopPoint = Math.floor(Math.random()*maxW);
+    console.log(poopPoint);
+    ctx.strokeStyle="red"
+
+}
+
+function poopDraw(){
+    ctx.beginPath();
+    ctx.arc(poopPoint, 30+i, 30, 0, Math.PI * 2);
+    ctx.stroke();
+}
+
 function mouse(e){
     const mouseX = e.offsetX; 
     const mouseY = e.offsetY; 
     x = mouseX-25;
-
+    
 }
 
 function draw(){
@@ -22,12 +39,18 @@ function draw(){
     ctx.beginPath();
     ctx.drawImage(img, x,y, 50,100);
     ctx.stroke();
+    poopDraw();
+    if(i<maxH){
+        i=i+2;
+    }else{
+        poop();
+    }
 }
 
 function init(){
     canvas.addEventListener("mousemove",mouse)
     setInterval(draw,1);
-
+    poop();
 }
 
 init();
